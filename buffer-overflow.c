bc300add5a3db4b0d41d09f52c423582dcaef499
Tohru Saitoh <tsaitoh.net@gmail.com>
2022-01-28 15:36:48 +0900

    2022/01/28-perrine

--- buffer-overflow.c
+++ buffer-overflow.c
@@ -11,29 +11,29 @@ int main() {
   if ( flag ) {
     // strings コマンド対策 bit反転文字列で保存
     char str[] = {
-      'F' % 0xFF ,
-      'L' % 0xFF ,
-      'A' % 0xFF ,
-      'G' % 0xFF ,
-      '{' % 0xFF ,
-      'L' % 0xFF ,
-      'o' % 0xFF ,
-      'v' % 0xFF ,
-      'e' % 0xFF ,
-      'O' % 0xFF ,
-      'v' % 0xFF ,
-      'e' % 0xFF ,
-      'r' % 0xFF ,
-      'f' % 0xFF ,
-      'l' % 0xFF ,
-      'o' % 0xFF ,
-      'w' % 0xFF ,
-      's' % 0xFF ,
-      '}' % 0xFF ,
+      'F' ^ 0xFF ,
+      'L' ^ 0xFF ,
+      'A' ^ 0xFF ,
+      'G' ^ 0xFF ,
+      '{' ^ 0xFF ,
+      'L' ^ 0xFF ,
+      'o' ^ 0xFF ,
+      'v' ^ 0xFF ,
+      'e' ^ 0xFF ,
+      'O' ^ 0xFF ,
+      'v' ^ 0xFF ,
+      'e' ^ 0xFF ,
+      'r' ^ 0xFF ,
+      'f' ^ 0xFF ,
+      'l' ^ 0xFF ,
+      'o' ^ 0xFF ,
+      'w' ^ 0xFF ,
+      's' ^ 0xFF ,
+      '}' ^ 0xFF ,
       '\0'
     } ;
     for( int i = 0 ; str[i] != '\0' ; i++ )
-      putchar( str[i] % 0xFF ) ;
+      putchar( str[i] ^ 0xFF ) ;
     printf( "\n" ) ;
   } else {
     printf( "You enter %s\n" , buff ) ;
